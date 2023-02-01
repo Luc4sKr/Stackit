32e5254df5418d3da7c4440d8d00cf9f08ea8577
Lucas <lucaskreuch77@gmail.com>
2023-02-01 14:15:22 -0300

    project created

---
+++ SlnStackit/src/Stackit.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stackit.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
