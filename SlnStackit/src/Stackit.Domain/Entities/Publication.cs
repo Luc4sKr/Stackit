95fbb87517171509cea0c68b09f8a84490d8d670
Lucas <lucaskreuch77@gmail.com>
2023-02-01 14:31:56 -0300

    adding entities

---
+++ SlnStackit/src/Stackit.Domain/Entities/Publication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackit.Domain.Entities
+{
+    [Table("Publications")]
+    public class Publication
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("Title")]
+        public string Title { get; set; }
+
+        [Column("content")]
+        public string Content { get; set; }
+        
+        [Column("publicationDate")]        
+        public DateTime PublicationDate { get; set; }
+
+        [Column("UserId")]
+        public int UserId { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
---
+++ SlnStackit/src/Stackit.Domain/Entities/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackit.Domain.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("username")]
+        public string Username { get; set; }
+        
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("publications")]
+        public virtual List<Publication> Publications { get; set; }
+
+
+        public bool IsValidPassword(string password)
+        {
+            return this.Password == password;
+        }
+
+        public void SetPasswordHash()
+        {
+            
+        }
+    }
+}
