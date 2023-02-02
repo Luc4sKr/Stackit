d12196f79f7a534a5f9e7dfb93cf551e2aaa7651
Lucas <lucaskreuch77@gmail.com>
2023-02-01 22:53:16 -0300

    more repositories

---
+++ SlnStackit/src/Stackit.Infra.Data/Repositories/PublicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Stackit.Domain.Entities;
+using Stackit.Domain.IRepositories;
+using Stackit.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackit.Infra.Data.Repositories
+{
+    public class PublicationRepository : BaseRepository<Publication>, IPublicationRepotitory
+    {
+        private readonly SQLServerContext _context;
+
+        public PublicationRepository(SQLServerContext context) : base(context) 
+        {
+            _context = context;
+        }
+    }
+}
---
+++ SlnStackit/src/Stackit.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Stackit.Domain.Entities;
+using Stackit.Domain.IRepositories;
+using Stackit.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackit.Infra.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        private readonly SQLServerContext _context;
+
+        public UserRepository(SQLServerContext context) : base(context) 
+        {
+            _context = context;
+        }
+    }
+}
