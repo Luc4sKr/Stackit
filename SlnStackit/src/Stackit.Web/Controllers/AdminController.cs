091a303818e2e5d280424bca140dd0d0fab96ee0
Lucas <lucaskreuch77@gmail.com>
2023-02-02 14:44:40 -0300

    more views and controllers

---
+++ SlnStackit/src/Stackit.Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Stackit.Web.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [Route("dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
